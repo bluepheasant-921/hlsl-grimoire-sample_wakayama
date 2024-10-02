0159b5fba92006d2e4252d79edf353d84bfc78cd
GAME\232e004 <waka0318.den@gmail.com>
2024-10-02 12:10:16 +0900

    繰り返し変化まで終了

--- Sample_09_09/Sample_09_09/main.cpp
+++ Sample_09_09/Sample_09_09/main.cpp
@@ -4,8 +4,8 @@
 // 頂点構造体
 struct SimpleVertex
 {
-    Vector4 pos;    // 頂点座標
-    Vector2 uv;     // UV座標
+	Vector4 pos;    // 頂点座標
+	Vector2 uv;     // UV座標
 };
 
 // 関数宣言
@@ -17,107 +17,149 @@ void InitPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader& 
 ///////////////////////////////////////////////////////////////////
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
-    // ゲームの初期化
-    InitGame(hInstance, hPrevInstance, lpCmdLine, nCmdShow, TEXT("Game"));
+	// ゲームの初期化
+	InitGame(hInstance, hPrevInstance, lpCmdLine, nCmdShow, TEXT("Game"));
 
-    //////////////////////////////////////
-    // ここから初期化を行うコードを記述する
-    //////////////////////////////////////
+	//////////////////////////////////////
+	// ここから初期化を行うコードを記述する
+	//////////////////////////////////////
 
-    // Spriteの初期化
-    // まずはSpriteクラスの初期化オブジェクトを作成する
-    SpriteInitData spriteInitData;
+	// Spriteの初期化
+	// まずはSpriteクラスの初期化オブジェクトを作成する
+	SpriteInitData spriteInitData;
 
-    // テクスチャのファイルパスを指定
-    spriteInitData.m_ddsFilePath[0] = "Assets/image/test.dds";
+	// テクスチャのファイルパスを指定
+	spriteInitData.m_ddsFilePath[0] = "Assets/image/test.dds";
 
-    // シェーダーファイルのパスを指定
-    spriteInitData.m_fxFilePath = "Assets/shader/sample.fx";
+	// シェーダーファイルのパスを指定
+	spriteInitData.m_fxFilePath = "Assets/shader/sample.fx";
 
-    // スプライトの幅と高さを指定
-    spriteInitData.m_width = FRAME_BUFFER_W;
-    spriteInitData.m_height = FRAME_BUFFER_H;
+	// スプライトの幅と高さを指定
+	spriteInitData.m_width = FRAME_BUFFER_W;
+	spriteInitData.m_height = FRAME_BUFFER_H;
 
-    // ワイプサイズ
-    float monochromeRate = 0.0f;
-    spriteInitData.m_expandConstantBuffer = &monochromeRate;
-    spriteInitData.m_expandConstantBufferSize = sizeof(monochromeRate);
+	// ワイプサイズ
+	float monochromeRate = 0.0f;
+	spriteInitData.m_expandConstantBuffer = &monochromeRate;
+	spriteInitData.m_expandConstantBufferSize = sizeof(monochromeRate);
 
-    // Spriteクラスのオブジェクトを定義して初期化する
-    Sprite test2D;
+	// Spriteクラスのオブジェクトを定義して初期化する
+	Sprite test2D;
 
-    // Init()に初期化オブジェクトを渡して初期化する
-    test2D.Init(spriteInitData);
+	// Init()に初期化オブジェクトを渡して初期化する
+	test2D.Init(spriteInitData);
 
-    //////////////////////////////////////
-    // 初期化を行うコードを書くのはここまで！！！
-    //////////////////////////////////////
-    auto& renderContext = g_graphicsEngine->GetRenderContext();
+	//課題
+	float addNumber = 0.01f;
+	bool plus = true;
 
-    // ここからゲームループ
-    while (DispatchWindowMessage())
-    {
-        // 1フレームの開始
-        g_engine->BeginFrame();
+	//////////////////////////////////////
+	// 初期化を行うコードを書くのはここまで！！！
+	//////////////////////////////////////
+	auto& renderContext = g_graphicsEngine->GetRenderContext();
 
-        //////////////////////////////////////
-        // ここから絵を描くコードを記述する
-        //////////////////////////////////////
-        // ワイプサイズを増やして少しずつワイプさせる
-        monochromeRate += 0.01f;
-        if (monochromeRate > 1.0f) {
-            monochromeRate = 1.0f;
-        }
-        // スプライトのドローコールを実行する
-        test2D.Draw(renderContext);
+	// ここからゲームループ
+	while (DispatchWindowMessage())
+	{
+		// 1フレームの開始
+		g_engine->BeginFrame();
 
-        //////////////////////////////////////
-        // 絵を描くコードを書くのはここまで！！！
-        //////////////////////////////////////
-        // 1フレーム終了
-        g_engine->EndFrame();
-    }
-    return 0;
+		//////////////////////////////////////
+		// ここから絵を描くコードを記述する
+		//////////////////////////////////////
+		// ワイプサイズを増やして少しずつワイプさせる
+		/*
+		monochromeRate += 0.01f;
+		if (monochromeRate > 1.0f) {
+			monochromeRate = 1.0f;
+		}
+		*/
+
+		//課題（点滅）
+		/*
+		if (monochromeRate == 0)
+		{
+			monochromeRate = 1;
+		}
+		else if (monochromeRate == 1)
+		{
+			monochromeRate = 0;
+		}
+		*/
+
+		//課題（徐々に変化の繰り返し）
+		monochromeRate += addNumber;
+		if (plus == true)
+		{
+			if (monochromeRate > 1.0f)
+			{
+				monochromeRate = 1.0f;
+				addNumber = -1 * addNumber;
+				plus = false;
+			}
+
+		}
+		if (plus == false)
+		{
+			if (monochromeRate < 0.0f)
+			{
+				monochromeRate = 0;
+				addNumber = -1 * addNumber;
+				plus = true;
+			}
+		}
+
+
+		// スプライトのドローコールを実行する
+		test2D.Draw(renderContext);
+
+		//////////////////////////////////////
+		// 絵を描くコードを書くのはここまで！！！
+		//////////////////////////////////////
+		// 1フレーム終了
+		g_engine->EndFrame();
+	}
+	return 0;
 }
 
 // ルートシグネチャの初期化
 void InitRootSignature(RootSignature& rs)
 {
-    rs.Init(D3D12_FILTER_MIN_MAG_MIP_LINEAR,
-            D3D12_TEXTURE_ADDRESS_MODE_WRAP,
-            D3D12_TEXTURE_ADDRESS_MODE_WRAP,
-            D3D12_TEXTURE_ADDRESS_MODE_WRAP);
+	rs.Init(D3D12_FILTER_MIN_MAG_MIP_LINEAR,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+		D3D12_TEXTURE_ADDRESS_MODE_WRAP);
 }
 
 // パイプラインステートの初期化
 void InitPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader& vs, Shader& ps)
 {
-    // 頂点レイアウトを定義する
-    D3D12_INPUT_ELEMENT_DESC inputElementDescs[] =
-    {
-        { "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
-        { "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 16, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
-    };
+	// 頂点レイアウトを定義する
+	D3D12_INPUT_ELEMENT_DESC inputElementDescs[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 16, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+	};
 
-    // パイプラインステートを作成
-    D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = { 0 };
-    psoDesc.InputLayout = { inputElementDescs, _countof(inputElementDescs) };
-    psoDesc.pRootSignature = rs.Get();
-    psoDesc.VS = CD3DX12_SHADER_BYTECODE(vs.GetCompiledBlob());
-    psoDesc.PS = CD3DX12_SHADER_BYTECODE(ps.GetCompiledBlob());
-    psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
-    psoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;
-    psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
-    psoDesc.DepthStencilState.DepthEnable = FALSE;
-    psoDesc.DepthStencilState.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO;
-    psoDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
-    psoDesc.DepthStencilState.StencilEnable = FALSE;
-    psoDesc.SampleMask = UINT_MAX;
-    psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
-    psoDesc.NumRenderTargets = 1;
-    psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
-    psoDesc.DSVFormat = DXGI_FORMAT_D32_FLOAT;
-    psoDesc.SampleDesc.Count = 1;
-    psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
-    pipelineState.Init(psoDesc);
+	// パイプラインステートを作成
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC psoDesc = { 0 };
+	psoDesc.InputLayout = { inputElementDescs, _countof(inputElementDescs) };
+	psoDesc.pRootSignature = rs.Get();
+	psoDesc.VS = CD3DX12_SHADER_BYTECODE(vs.GetCompiledBlob());
+	psoDesc.PS = CD3DX12_SHADER_BYTECODE(ps.GetCompiledBlob());
+	psoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+	psoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_NONE;
+	psoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+	psoDesc.DepthStencilState.DepthEnable = FALSE;
+	psoDesc.DepthStencilState.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ZERO;
+	psoDesc.DepthStencilState.DepthFunc = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+	psoDesc.DepthStencilState.StencilEnable = FALSE;
+	psoDesc.SampleMask = UINT_MAX;
+	psoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	psoDesc.NumRenderTargets = 1;
+	psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+	psoDesc.DSVFormat = DXGI_FORMAT_D32_FLOAT;
+	psoDesc.SampleDesc.Count = 1;
+	psoDesc.RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
+	pipelineState.Init(psoDesc);
 }
--- Sample_09_10/Sample_09_10/Assets/shader/sample.fx
+++ Sample_09_10/Sample_09_10/Assets/shader/sample.fx
@@ -24,9 +24,9 @@ Texture2D<float4> colorTexture : register(t0); // カラーテクスチャ
 sampler Sampler : register(s0);
 
 // ハッシュ関数
-float hash( float n )
+float hash(float n)
 {
-    return frac(sin(n)*43758.5453);
+    return frac(sin(n) * 43758.5453);
 }
 
 // 3次元ベクトルからシンプレックスノイズを生成する関数
@@ -40,9 +40,9 @@ float SimplexNoise(float3 x)
     float n = p.x + p.y * 57.0 + 113.0 * p.z;
 
     return lerp(lerp(lerp(hash(n + 0.0), hash(n + 1.0), f.x),
-                     lerp(hash(n + 57.0), hash(n + 58.0), f.x), f.y),
-                lerp(lerp(hash(n + 113.0), hash(n + 114.0), f.x),
-                     lerp(hash(n + 170.0), hash(n + 171.0), f.x), f.y), f.z);
+        lerp(hash(n + 57.0), hash(n + 58.0), f.x), f.y),
+        lerp(lerp(hash(n + 113.0), hash(n + 114.0), f.x),
+            lerp(hash(n + 170.0), hash(n + 171.0), f.x), f.y), f.z);
 }
 
 PSInput VSMain(VSInput In)
@@ -56,6 +56,18 @@ PSInput VSMain(VSInput In)
 float4 PSMain(PSInput In) : SV_Target0
 {
     // step-1 シンプレックスノイズを利用してノイズ加工を行う
+    // シンプレックスノイズを使用して、0～1の乱数を取得
+    float t = SimplexNoise(In.pos.xyz);
 
-    return color;
+// ノイズの値の範囲を0～1から-1～1に変換
+t = (t - 0.5f) * 2.0f;
+
+// UV座標にノイズを加える。0.01fはノイズの強さ
+// この数値を大きくするとノイズが大きくなる
+float2 uv = In.uv + t * 0.01f;
+
+// ずらしたUV座標を利用して、カラーをサンプリングする
+float4 color = colorTexture.Sample(Sampler, uv);
+
+return color;
 }
